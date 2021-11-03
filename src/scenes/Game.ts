79c03d4efb2afd20aebd6029e072136d02aec363
Felipe Mano <manofgpa@gmail.com>
2021-11-03 08:42:21 -0300

    Add camera

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -21,22 +21,27 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
+    // Map creation
     const map = this.make.tilemap({ key: 'mainMap' })
     const tileset = map.addTilesetImage('tiles', 'tiles')
     const tileset2 = map.addTilesetImage('tiles2', 'tiles2')
 
     const skyLayer = map.createLayer('sky', tileset2)
     const mainLayer = map.createLayer('main', tileset)
-
+    // Map collision
     mainLayer.setCollisionByProperty({ collides: true })
     skyLayer.setCollisionByProperty({ collides: true })
 
+    // Colission debug
     const debugGraphics = this.add.graphics().setAlpha(0.75)
     mainLayer.renderDebug(debugGraphics, {
       tileColor: null,
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
       faceColor: new Phaser.Display.Color(40, 39, 37, 255),
     })
+
+    this.cameras.main.setZoom(1)
+    this.cameras.main.centerOn(0, 0)
 
     this.platforms = this.physics.add.staticGroup()
 
@@ -46,6 +51,10 @@ export default class Game extends Phaser.Scene {
 
     this.physics.add.collider(this.player, mainLayer)
     // this.physics.add.collider(this.player, this.platforms)
+
+    // Camera
+    this.cameras.main.setBounds(0, 0, 2048, 500)
+    this.cameras.main.startFollow(this.player, false, 1, 0)
 
     this.anims.create({
       key: 'left',
